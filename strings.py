f90f9cdea6102579afbaf756cfc0a334e6d3a015
Bertoldo Klinger <bertoldokbrj@gmail.com>
2024-10-27 21:46:28 -0300

    feat: add string methods impl

---
+++ strings.py
@@ -0,0 +1,36 @@
+s = 'minha primeira string' ## Declarando variável
+
+s = 'teste' * 6 ## 'testetestetestetestetesteteste'
+
+
+'bertoldo'.replace('oldo','ando') ## 'bertando'
+
+'bertoldo'.upper() ## 'BERTOLDO'
+
+'bertoldo'.lower() ## 'bertoldo'
+
+'bertoldo'.startswith('bert') ## True
+
+'bertoldo'.endswith('ldo') ## True
+
+'bertoldo'.split() ## ["bertoldo"]
+
+' bertoldo '.strip() ## 'bertoldo' => é o trim() do js,tem o rstrip e o lstrip também, right e left.
+
+'bertoldo'.title() ## 'Bertoldo' => É o capitalize do js
+
+s = '0123456789'
+s[0] ## '0' => Acessando indícies de uma string
+s[5] ## '5'
+s[0:5] ## '01234'
+s[::-1] ## '9876543210' => Inverte uma string de forma simples e otimizada
+
+s[2] = '3' ## Dentro de uma string, os valor são imutáveis, não pode alterar
+
+
+
+
+
+
+
+
